9d6cab27fe7955bf8e82b8c01938fcc13597b81d
xuai05 <45277994+xuai05@users.noreply.github.com>
2019-04-08 19:39:41 +0800

    Add files via upload

---
+++ 操作系统实验一/1-1-2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+	pid_t pid;
+	asm volatile(
+		"mov $0,%%ebx\n\t"
+		"mov $0x14,%%eax\n\t"
+		"int $0x80"
+		:"=m"(pid)
+		);
+	printf("%d\n", pid);
+	return 0;
+}
